7e2edd7fa4c4c2932ffcd2908f7abea829ebd09f
George Anderson <guimaraes.george@gmail.com>
2013-06-03 20:42:28 -0300

    Subindo projeto inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Moedalimpa::Application.config.secret_token = 'b8e562c0e61b719d6062c76d02ed4f4e09c1796a34b4688b4d3526cbe50f418e6ba9207da483667a523925ca173e3b44736f3efe4a9fd21230e28026c3b493dc'
